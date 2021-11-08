c80a6a194e07b7cca7128a01b99d56267c18fe22
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-08 14:40:50 -0500

    game status showing and winner

--- game.rb
+++ game.rb
@@ -7,4 +7,12 @@ class Game
   def next_Player
     @player_turn += 1
   end
+  
+  def game_status(player1, player2)
+    puts "#{player1.name}: #{player1.lives}/3      #{player2.name}: #{player2.lives}/3"
+  end
+
+  def winner(player1, player2)
+    puts (player1.lives > player2.lives) ? "#{player1.name} Wins with score #{player1.lives}/3" : "#{player2.name} Wins with score #{player2.lives}/3"
+  end
 end
