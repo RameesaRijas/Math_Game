cfa45ee0d4db8e14d5221c6e6efcd7bc4f0bca0e
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-11-08 13:22:48 -0500

    Make to accept player nmes from user, Needs refactor

--- main.rb
+++ main.rb
@@ -1,1 +1,19 @@
 require './players'
+
+# needs refactor
+#just starting 
+puts "Welcome to the Game"
+puts "Are you ready to Start"
+puts "Yes press Enter or wants to quit press cntrl+c"
+$stdin.gets
+puts "Ok then Let's Start"
+puts "Player1 : What is your Name?"
+
+player1 = Player.new(gets.chomp)
+puts "Player2 : What is your Name?"
+player2 = Player.new(gets.chomp)
+puts "Hi #{player1.name} and #{player2.name} Let's Start our Game"
+
+
+
+
